0cc5608c435c21d0632685dec5dd7d180cf067ed
Siddhesh Bait <sidbait@gmail.com>
2021-11-16 13:01:32 +0530

    UI chages

---
+++ ovpl_frontend/src/app/pages/teams/addTeams/teams.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-user",
+  templateUrl: "addTeams.component.html"
+})
+export class UserComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
---
+++ ovpl_frontend/src/app/pages/teams/teams.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-tables",
+  templateUrl: "teams.component.html"
+})
+export class TeamsComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
